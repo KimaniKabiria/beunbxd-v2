47eba269cf15b424df53abe7f245c755da98e8e0
Alvin Kimani <hello@kimmi.dev>
2022-08-27 15:55:57 +0300

    Finished migration of design Page

---
+++ app/components/design/DesignHero.tsx
@@ -0,0 +1,60 @@
+
+import Typewriter from 'typewriter-effect'
+
+import designHero from "../../assets/images/super-woman.svg"
+
+export default function DesignHero (){
+
+    return (
+        <section className="w-full">
+            <div className="flex flex-col mx-auto px-6 lg:px-12 lg:py-8 lg:flex mb-12">
+                <div className="flex flex-col">
+                    <div className="flex flex-col lg:flex-row lg:bg-gradient-to-r lg:from-taran-orange lg:to-taran-blue bg-gradient-to-b from-taran-blue to-taran-orange text-unbxd-bg rounded-2xl">
+                        <div className="block mx-auto lg:order-last lg:-ml-0 p-12">
+                            <div className="flex items-end justify-center">
+                                <img
+                                className="block lg:p-8 w-2/3 h-full"
+                                src={designHero}
+                                alt="Unbxd Box"
+                                />
+                            </div>
+                        </div>
+                        <div className="flex flex-row justify-center items-center mx-auto lg:w-1/2 px-8 lg:px-12  pb-8 lg:pb-0">
+                            <div className="flex flex-col lg:pl-8">
+                                <h1 className="text-6xl lg:text-8xl font-base">Unbxing</h1>
+                                    <h1 className="text-5xl font-bold mt-4 lg:mt-8">
+                                        <Typewriter			
+                                            options={{
+                                                loop: true,
+                                            }}
+                                            onInit={(typewriter) => {
+                                                typewriter
+                                                    .pauseFor(500)
+                                                    .typeString("<strong>UI</strong>")
+                                                    .pauseFor(3000)
+                                                    .deleteChars(2)
+                                                    .typeString("<strong>UX</strong>")
+                                                    .pauseFor(3000)
+                                                    .deleteChars(2)
+                                                    .typeString("<strong>DESIGN</strong>")
+                                                    .pauseFor(3000)
+                                                    .deleteChars(7)
+                                                    .start();
+                                            }}
+                                        />
+                                    </h1>
+                                <p className="mt-8 text-xl lg:text-3xl font-semibold">
+                                    Intuitive
+                                    <span className="text-taran-blue font-semibold border-b-2 border-taran-blue"> Design </span>
+                                    is how we give the user new
+                                    <span className="text-taran-blue font-semibold border-b-2 border-taran-blue"> Superpowers </span>
+                                    !
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
--- app/routes/design/index.tsx
+++ app/routes/design/index.tsx
@@ -1,15 +1,13 @@
-import AboutHero from "~/components/about/AboutHero";
-import AboutCompany from "~/components/about/AboutComapany";
-import WhyUs from "~/components/about/WhyUs";
-import HowWeDo from "~/components/about/HowWeDo";
+import DesignHero from "~/components/design/DesignHero";
 
 export default function DesignIndex() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }} className="w-full">
-      <AboutHero />
-      <AboutCompany />
-      <WhyUs />
-      <HowWeDo />
+      <DesignHero />
+
+      <div className="flex lg:p-32 p-12 justify-center items-center">
+            <h1 className="lg:text-6xl text-3xl">You deserve a <span className="text-taran-blue underline font-medium"> dedicated partner</span> to help you design <span className="text-taran-orange underline font-medium"> growth-oriented products</span> that create impact for your customers.</h1>
+        </div>
     </div>
   );
 }
