006bf7a49fbfd0dc9ac948e53b7210969e3718d2
Alvin Kimani <hello@kimmi.dev>
2022-09-05 20:13:18 +0300

    About Company UI Upgrade

--- app/components/about/AboutComapany.tsx
+++ app/components/about/AboutComapany.tsx
@@ -13,14 +13,14 @@ export default function AboutCompany() {
     }, []);
 return (
   <section className="flex items-center mx-auto lg:w-11/12">
-        <div className="flex bg-gradient-to-r from-taran-orange to-taran-blue text-white mx-auto items-center justify-center mt-4 mb-16 py-6 px-4 lg:py-0 rounded-2xl">
+        <div className="flex bg-gradient-to-r from-taran-orange to-taran-blue text-white mx-auto items-center justify-center mt-4 mb-16 py-6 px-4 lg:py-0 rounded-2xl shadow-2xl">
             <div className="flex flex-col xl:flex-row p-4 lg:p-20">
                 <div className="xl:w-2/3">
                     <h1 className="flex items-center justify-center">
                         <div id="team"/>
                     </h1>
                 </div>
-                <div className="flex mt-4 xl:mt-0 w-full xl:w-1/2 items-center justify-center lg:px-20 pb-6">
+                <div className="flex mt-4 xl:mt-0 w-full xl:w-1/2 items-center justify-center lg:pl-4 xl:px-12 pb-6">
                     <div className="">
                         <p className="mt-4 lg:text-xl text-base text-center lg:text-left leading-relaxed">
                             We are a well oiled machine comprised of a team of talented individuals who are gurus in their field of work and masters of their skill.
