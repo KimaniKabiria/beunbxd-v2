b166571b45f13c8b61537cf89fcd513667625460
Alvin Kimani <hello@kimmi.dev>
2022-09-05 20:15:31 +0300

    WHy Us UI Upgrade

--- app/components/about/WhyUs.tsx
+++ app/components/about/WhyUs.tsx
@@ -2,7 +2,7 @@ import { BsCheck2Circle } from "react-icons/bs"
 
 export default function WhyUs(){
 return (
-    <section className="flex flex-col mx-auto bg-white -mt-20 lg:-mt-24 lg:w-11/12 rounded-2xl">
+    <section className="flex flex-col mx-auto lg:w-11/12 rounded-2xl">
         <div className="flex flex-col p-12 lg:p-20">
             <div className="flex p-3">
                 <h1 className="inline-block text-taran-blue p-3 lg:text-4xl text-3xl font-bold mb-2 lg:mb-4 border-b-2 border-taran-orange">
@@ -19,14 +19,14 @@ return (
             </div>
             <div className="flex flex-col xl:px-20 py-8">
                 <div className="flex flex-col lg:grid lg:grid-cols-2 lg:gap-2 lg:w-full justify-center items-center">
-                    <div className="flex flex-col w-full p-12 bg-unbxd-bg rounded-2xl shadow-2xl hover:shadow-xl">
+                    <div className="flex flex-col w-full p-12 bg-white rounded-2xl shadow-2xl hover:shadow-xl">
                         <div className="flex flex-row font-bold">
                             <BsCheck2Circle className="w-8 h-8 text-taran-blue" />
                             <p className="px-4 text-xl lg:text-2xl text-taran-orange">Adaptable Designs</p>
                         </div>
                         <p className="mt-4 lg:pl-12">We love to create. Our designers have excuisite taste and a good eye for something bespoke. So you should expect that level of design work on your project.</p>
                     </div>
-                    <div className="flex flex-col w-full p-12 mt-6 lg:mt-0 bg-unbxd-bg rounded-2xl shadow-2xl hover:shadow-xl">
+                    <div className="flex flex-col w-full p-12 mt-6 lg:mt-0 bg-white rounded-2xl shadow-2xl hover:shadow-xl">
                         <div className="flex flex-row font-bold">
                             <BsCheck2Circle className="w-8 h-8 text-taran-blue" />
                             <p className="px-4 text-xl lg:text-2xl text-taran-orange">Great Developers</p>
@@ -35,14 +35,14 @@ return (
                     </div>
                 </div>
                 <div className="flex flex-col lg:grid lg:grid-cols-2 lg:gap-2 lg:w-full justify-center items-center mt-6 lg:mt-8">
-                    <div className="flex flex-col w-full p-12 bg-unbxd-bg rounded-2xl shadow-2xl hover:shadow-xl">
+                    <div className="flex flex-col w-full p-12 bg-white rounded-2xl shadow-2xl hover:shadow-xl">
                         <div className="flex flex-row font-bold">
                             <BsCheck2Circle className="w-8 h-8 text-taran-blue" />
                             <p className="px-4 text-xl lg:text-2xl text-taran-orange">Always there to help you</p>
                         </div>
                         <p className="mt-4 lg:pl-12">Building strong, long-lasting relations is crucial for our business. We want you to succeed and grow with us. We`ll do our best to make it happen.</p>
                     </div>
-                    <div className="flex flex-col w-full p-12 mt-6 lg:mt-0 bg-unbxd-bg rounded-2xl shadow-2xl hover:shadow-xl">
+                    <div className="flex flex-col w-full p-12 mt-6 lg:mt-0 bg-white rounded-2xl shadow-2xl hover:shadow-xl">
                         <div className="flex flex-row font-bold">
                             <BsCheck2Circle className="w-8 h-8 text-taran-blue" />
                             <p className="px-4 text-xl lg:text-2xl text-taran-orange">Faster than Agile</p>
