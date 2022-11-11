e3402c3fef6634e581b50dfe547ac404bb16c034
Kimani Kabiria <kabiriakimani@gmail.com>
2022-11-11 16:55:52 +0300

    Started Redesign of design page

--- app/components/design/DesignHero.tsx
+++ app/components/design/DesignHero.tsx
@@ -1,61 +1,66 @@
 import React from "react";
-import Typewriter from 'typewriter-effect'
+import Typewriter from "typewriter-effect";
 import lottie from "lottie-web";
+import { RainbowHighlight } from "../includes/RainbowHighlight";
 
-import Design from "../../assets/lotties/design.json"
+import Design from "../../assets/lotties/design.json";
 
-export default function DesignHero (){
-    React.useEffect(() => {
-      lottie.loadAnimation({
-        container: document.querySelector("#design-lottie"),
-        animationData: Design
-      });
-    }, []);
+export default function DesignHero() {
+  React.useEffect(() => {
+    lottie.loadAnimation({
+      container: document.querySelector("#design-lottie"),
+      animationData: Design,
+    });
+  }, []);
 
-    return (
-        <section className="flex flex-col mx-auto lg:w-11/12 px-6 lg:px-20 justify-center items-center lg:bg-gradient-to-r lg:from-taran-orange lg:to-taran-blue bg-gradient-to-b from-taran-blue to-taran-orange lg:mt-24 lg:rounded-2xl">
-            <div className="flex flex-col mx-auto px-6 lg:px-12 lg:flex ">
-                <div className="flex flex-col">
-                    <div className="flex flex-col lg:flex-row  text-unbxd-bg rounded-2xl">
-                        <div className="block mx-auto lg:order-last lg:-ml-0 lg:p-12">
-                            <div id="design-lottie"  style={{ height: 560 }}/>
-                        </div>
-                        <div className="flex flex-row justify-center items-center mx-auto lg:w-1/2 px-8 lg:px-12  pb-8 lg:pb-0">
-                            <div className="flex flex-col lg:pl-8">
-                                <h1 className="text-6xl lg:text-8xl font-base">Unbxing</h1>
-                                    <h1 className="text-5xl font-bold mt-4 lg:mt-8">
-                                        <Typewriter			
-                                            options={{
-                                                loop: true,
-                                            }}
-                                            onInit={(typewriter) => {
-                                                typewriter
-                                                    .pauseFor(500)
-                                                    .typeString("<strong>UI</strong>")
-                                                    .pauseFor(3000)
-                                                    .deleteChars(2)
-                                                    .typeString("<strong>UX</strong>")
-                                                    .pauseFor(3000)
-                                                    .deleteChars(2)
-                                                    .typeString("<strong>DESIGN</strong>")
-                                                    .pauseFor(3000)
-                                                    .deleteChars(7)
-                                                    .start();
-                                            }}
-                                        />
-                                    </h1>
-                                <p className="mt-8 text-xl lg:text-4xl font-semibold">
-                                    Beautiful
-                                    <span className="text-taran-blue font-semibold border-b-2 border-taran-blue"> Solutions </span>
-                                    to complex
-                                    <span className="text-taran-blue font-semibold border-b-2 border-taran-blue"> Problems </span>
-                                    !
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-};
+  const colors = ["#009193", "#23074d", "#cc5333", "#3B82F6"];
+
+  return (
+    <section className="flex flex-col w-full mx-auto px-6 lg:px-20 justify-center items-center mt-24 lg:mt-48 lg:rounded-2xl">
+      <div className="flex flex-col justify-center items-center text-taran-blue rounded-2xl w-full">
+        <div className="flex flex-col justify-center items-center mx-auto px-8 lg:px-12 w-full pb-8 lg:pb-0">
+          <div className="flex flex-col lg:flex-row justify-center items-center">
+            <h1 className="text-5xl lg:text-8xl font-medium pl-4">Unbxing</h1>
+            <h1 className="text-5xl lg:text-8xl font-medium text-taran-orange">
+              <Typewriter
+                options={{
+                  loop: true,
+                }}
+                onInit={(typewriter) => {
+                  typewriter
+                    .pauseFor(500)
+                    .typeString("<strong>UI</strong>")
+                    .pauseFor(3000)
+                    .deleteChars(2)
+                    .typeString("<strong>UX</strong>")
+                    .pauseFor(3000)
+                    .deleteChars(2)
+                    .typeString("<strong>DESIGN</strong>")
+                    .pauseFor(3000)
+                    .deleteChars(7)
+                    .start();
+                }}
+              />
+            </h1>
+          </div>
+          <div className="flex justify-center items-center w-11/12 lg:w-full">
+            <p className="mt-8 text-2xl text-center lg:text-4xl font-semibold">
+              Beautiful{" "}
+              <RainbowHighlight color={colors[2]}>
+                <span className="text-white">{" "}Solutions{" "}</span>
+              </RainbowHighlight>{" "}
+              to complex{" "}
+              <RainbowHighlight color={colors[2]}>
+                <span className="text-white">{" "}Problems{" "}</span>
+              </RainbowHighlight>{" "}
+              !
+            </p>
+          </div>
+        </div>
+        <div className="flex mx-auto">
+          <div id="design-lottie" style={{ height: 560 }} />
+        </div>
+      </div>
+    </section>
+  );
+}
--- app/routes/design/index.tsx
+++ app/routes/design/index.tsx
@@ -1,22 +1,53 @@
 import DesignHero from "~/components/design/DesignHero";
 import WhatIsUiUx from "~/components/design/WhatIsUiUX";
 import DesignProcess from "~/components/design/DesignProcess";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 export default function DesignIndex() {
+  const colors = ["#fcf6f5", "#23074d", "#cc5333", "#3B82F6"];
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}  className="w-full -mt-20">
-      <DesignHero />
+    <div
+      style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}
+      className="w-full -mt-20"
+    >
+      <RoughNotationGroup show={true}>
+        <DesignHero />
 
-      <section className="bg-unbxd-bg flex items-center mx-auto lg:w-11/12 rounded-2xl">
-        <div className="flex lg:p-32 p-12 justify-center items-center">
-            <h1 className="lg:text-6xl text-3xl">You deserve a <span className="text-taran-blue underline font-medium"> dedicated partner</span> to help you design <span className="text-taran-orange underline font-medium"> growth-oriented products</span> that create impact for your customers.</h1>
-        </div>
-      </section>
+        <section className="flex bg-gradient-to-r from-taran-orange to-taran-blue text-white mx-auto items-center justify-start mt-4 mb-16 px-4 py-20 rounded-3xl">
+          <div className="flex lg:p-32 p-12 justify-center items-center">
+            <h1 className="lg:text-6xl text-3xl">
+              You deserve a{" "}
+              <RoughNotation
+                type="underline"
+                color={colors[0]}
+                padding={0}
+                iterations={5}
+              >
+                <span className="text-taran-blue font-medium">
+                  {" "}
+                  dedicated partner
+                </span>
+              </RoughNotation>{" "}
+              to help you design{" "}
+              <RoughNotation
+                type="underline"
+                color={colors[0]}
+                padding={0}
+                iterations={5}
+              >
+                <span className="text-taran-blue font-medium">
+                  growth-oriented products
+                </span>
+              </RoughNotation>{" "}
+              that create impact for your customers.
+            </h1>
+          </div>
+        </section>
 
-      <WhatIsUiUx />
-      
-      <DesignProcess />
+        <WhatIsUiUx />
 
+        <DesignProcess />
+      </RoughNotationGroup>
     </div>
   );
 }
