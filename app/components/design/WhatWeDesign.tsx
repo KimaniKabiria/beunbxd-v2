4378c43de4f458441e0d212fa2cbb23adb9a1e0f
Kimani Kabiria <kabiriakimani@gmail.com>
2022-11-14 12:16:13 +0300

    Finished What We Design Section

--- app/components/design/WhatWeDesign.tsx
+++ app/components/design/WhatWeDesign.tsx
@@ -22,11 +22,12 @@ export default function WhatWeDesign() {
               <div id="ui-ux" />
             </div>
             <div className="flex flex-col lg:w-2/3 p-8 lg:p-0 items-center justify-center text-center">
-              <h1 className="text-xl lg:text-3xl py-6 text-unbxd-bg font-medium">
+              <h1 className="text-3xl lg:text-4xl py-6 text-unbxd-bg font-medium">
                 <RainbowHighlight color={colors[2]}>
                   So, what do we Design?
                 </RainbowHighlight>
               </h1>
+              {/* UI Design */}
               <>
                 <button
                   data-popover-target="popover-default"
@@ -65,6 +66,7 @@ export default function WhatWeDesign() {
                   <div data-popper-arrow></div>
                 </div>
               </>
+              {/* UX Design */}
               <>
                 <button
                   data-popover-target="popover-ux"
@@ -81,7 +83,7 @@ export default function WhatWeDesign() {
                 >
                   <div class="p-3 space-y-2">
                     <h3 class="font-semibold text-xl">
-                      What is User Interface Design?
+                      What is User Experience Design?
                     </h3>
                     <p>
                       User experience (UX) design is the process design teams
@@ -90,6 +92,93 @@ export default function WhatWeDesign() {
                       design of the entire process of acquiring and integrating
                       the product, including aspects of branding, design,
                       usability and function.
+                    </p>
+                    <p>
+                      Our team of design experts conceptualize user experiences
+                      for your product that will give your client a feel of a
+                      personalised experince tailored for them.
+                    </p>
+                  </div>
+                  <div data-popper-arrow></div>
+                </div>
+              </>
+              {/* Web Design */}
+              <>
+                <button
+                  data-popover-target="popover-web"
+                  type="button"
+                  class="text-taran-blue bg-unbxd-bg border-2 border-taran-orange focus:ring-4 focus:outline-none focus:ring-taran-orange font-medium rounded-lg text-3xl px-5 py-2.5 text-center my-2"
+                >
+                  Web Design
+                </button>
+                <div
+                  data-popover
+                  id="popover-web"
+                  role="tooltip"
+                  class="inline-block absolute invisible z-10 w-72 text-sm font-light bg-gradient-to-r from-taran-orange to-taran-blue text-white rounded-lg border border-taran-blue shadow-sm opacity-0 transition-opacity duration-300"
+                >
+                  <div class="p-3 space-y-2">
+                    <h3 class="font-semibold text-xl">
+                      What is Web Design Design?
+                    </h3>
+                    <p>
+                      Web design is the art of planning and arranging content on
+                      a website so that it can be shared and accessed online
+                      with the world. A combination of aesthetic and functional
+                      elements, web design is what determines the look of a
+                      website—such as its colors, fonts, and graphics—as well as
+                      shaping the site’s structure and the users’ experience of
+                      it.
+                    </p>
+                    <p>
+                      As web artists, we make sure the end design talks about
+                      you and gives your clients a bespoke experience when using
+                      your website.
+                    </p>
+                  </div>
+                  <div data-popper-arrow></div>
+                </div>
+              </>
+              {/* Mobile Application Design */}
+              <>
+                <button
+                  data-popover-target="popover-mob"
+                  type="button"
+                  class="text-taran-blue bg-unbxd-bg border-2 border-taran-blue focus:ring-4 focus:outline-none focus:ring-taran-blue font-medium rounded-lg text-3xl px-5 py-2.5 text-center my-2"
+                >
+                  Mobile Application Design
+                </button>
+                <div
+                  data-popover
+                  id="popover-mob"
+                  role="tooltip"
+                  class="inline-block absolute invisible z-10 w-72 text-sm font-light bg-gradient-to-r from-taran-orange to-taran-blue text-white rounded-lg border border-taran-blue shadow-sm opacity-0 transition-opacity duration-300"
+                >
+                  <div class="p-3 space-y-2">
+                    <h3 class="font-semibold text-xl">
+                      What is Mobile Application Design?
+                    </h3>
+                    <p>
+                      App design is the look and feel of a mobile application.
+                      This includes all of the visual elements and interactive
+                      elements that impact how the app functions. 
+                    </p>
+                    <p>Mobile app
+                      design combines two concepts—UI and UX. The user interface
+                      (UI) encompasses the app’s look and feel. It includes
+                      color, font, app style, and similar design choices. The
+                      user experience (UX) design prioritizes the app’s
+                      functions and use. So many people have great app ideas.
+                    </p>
+                    <p>
+                      But without a high-quality UI and UX design, it will be
+                      tough to keep users coming back. Whether you’re building
+                      an ecommerce app, internal employee app, fitness app, or
+                      app in any category, the design always needs to be a top
+                      priority. That’s why it’s so important to invest in app
+                      design from the beginning. An investment now will actually
+                      save you money and ensure a higher ROI for your app down
+                      the road.
                     </p>
                   </div>
                   <div data-popper-arrow></div>
